1b461cc656fdbec960ebaa075977b6754fa0539b
OBigVee <adedejivictor98@gmail.com>
2021-10-27 01:59:01 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,17 @@
-#!/bin/sh
+#include "sort.h"
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+void bubble_sort(int *array, size_t size){
 
-void bubble_sort(int *array, size_t size);
+    for(unsigned int  n=1; n<size-1; ++n){
+        for ( unsigned num=0; num<size; ++num){
+            if (array[num] > array[num+1]){
+                int bigVal = array[num];
+                array[num] = array[num+1];
+                array[num+1] = bigVal;
+                print_array(array, size);
+            }
+        }
+    }
+
+
+}
