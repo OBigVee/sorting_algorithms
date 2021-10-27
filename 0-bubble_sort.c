6b019220d589bb9fa51c67c18211417a6a59cca1
Victor Adedeji <adedejivictor98@gmail.com>
2021-10-27 04:05:22 +0100

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,13 +12,13 @@ void bubble_sort(int *array, size_t size)
 size_t n, num;
 for (n = 0; n < size - 1; ++n)
 {
-for(num = 0; num < size; ++num)
+for (num = 0; num < size; ++num)
 {
-if (array[num] > array[num+1])
+if (array[num] > array[num + 1])
 {
 int bigVal = array[num];
-array[num] = array[num+1];
-array[num+1] = bigVal;
+array[num] = array[num + 1];
+array[num + 1] = bigVal;
 print_array(array, size);
 }
 }
