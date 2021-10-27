2656ab418c7093e3eceae7e75419cc3df3bc6661
Victor Adedeji <adedejivictor98@gmail.com>
2021-10-27 02:05:51 +0100

    update bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,10 +1,21 @@
 #include "sort.h"
 
-void bubble_sort(int *array, size_t size){
+/**
+ * bubble_sort - sorts an array in ascending order
+ * @array: Array pointer
+ * @size: Length of array
+ * Return; void
+ */
 
-    for(unsigned int  n=1; n<size-1; ++n){
-        for ( unsigned num=0; num<size; ++num){
-            if (array[num] > array[num+1]){
+void bubble_sort(int *array, size_t size)
+{
+
+    for(unsigned int  n=1; n<size-1; ++n)
+    {
+        for ( unsigned num=0; num<size; ++num)
+        {
+            if (array[num] > array[num+1])
+            {
                 int bigVal = array[num];
                 array[num] = array[num+1];
                 array[num+1] = bigVal;
